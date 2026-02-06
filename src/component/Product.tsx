3fd70bd09e1335c34e568ced9fa39cc176c7017e
Lavesh06 <kumarlavesh2001@gmail.com>
2026-02-06 10:01:45 +0530

    ajay/vedio

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -17,6 +17,12 @@ const images = [
   "/product4.jpg",
   "/product5.jpg",
   "/product6.jpg",
+ 
+  "/product10.png",
+  "/product11.png",
+  "/product12.png",
+  "/product13.png",
+  "/product14.png",
 ]
 
 const Product = () => {
@@ -30,7 +36,6 @@ const Product = () => {
   return (
     <section className="py-10 bg-white">
       <div className="max-w-7xl mx-auto px-3">
-
         <h2 className="text-3xl text-green-800 md:text-4xl font-bold text-center mb-6">
           Our Products
         </h2>
@@ -47,20 +52,17 @@ const Product = () => {
                 className="pl-2 basis-1/2 sm:basis-1/3 md:basis-1/4"
               >
                 <div className="relative w-full h-[140px] sm:h-[170px] md:h-[200px] rounded-full overflow-hidden border-2 border-green-600 shadow-xl">
-
                   <Image
                     src={src}
                     alt={`product-${index}`}
                     fill
                     className="object-cover"
                   />
-
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
-
       </div>
     </section>
   )
